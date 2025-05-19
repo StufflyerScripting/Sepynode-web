d7959df4e9bd1259b37d63debc344f26622e548d
StufflyerScripting <167232463+StufflyerScripting@users.noreply.github.com>
2025-05-19 21:39:13 +0200

    Create script.js

---
+++ src/script.js
@@ -0,0 +1,6 @@
+function showPage(id) {
+  document.querySelectorAll('.page').forEach(page => {
+    page.classList.remove('active');
+  });
+  document.getElementById(id).classList.add('active');
+}
